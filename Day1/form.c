c42478c8648955292bd9c80c42611cf64f2a05e3
100DaysOfCodeChallenge <94281803+BhavKush@users.noreply.github.com>
2022-10-05 23:24:48 +0530

    Create form.c
    In this file we write some CSS Property for Dummy Inquiry Form

---
+++ Day1/form.c
@@ -0,0 +1,50 @@
+*{
+    box-sizing: border-box;
+    margin: 10px;
+}
+
+body{
+    margin: 10px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 15px 30px;
+}
+
+header{
+    text-align:left;
+    font-size: 20px;
+    color: azure;
+    background-color: rgb(43, 128, 0);
+    margin: 0px;
+    border-radius: 6px;
+}
+
+.container{
+    background-color: #f2f2f2;
+    padding: 5px 15px 15px 20px;
+    border: 1px solid lightgray;
+    border-radius: 4px;
+}
+
+input[type="text"],
+input[type="email"],
+textarea,select{
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin: 10px;
+}
+
+input[type="submit"] {
+    background-color: #196005;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    width: 6%;
+    margin: 1px;
+}
+input[type="submit"]:hover{
+    background-color: #057478b6;
+}
