5d0e89e85b1ea0dd24885c25c116652630dbeadb
100DaysOfCodeChallenge <94281803+BhavKush@users.noreply.github.com>
2022-10-06 22:48:40 +0530

    Create Dummy Table,anchor,Iframe.h
    In this Git file i had included anchor tags,table tag,iframe tag.

---
+++ Day2/Dummy Table,anchor,Iframe.h
@@ -0,0 +1,159 @@
+/* Use of Table tag,Anchor tag,Iframe another website to this webpage */
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mine document</title>
+    <link rel="stylesheet" href="new.css">
+    <style>
+       
+    </style>
+    
+</head>
+<body>
+    
+    <h1>this is heading&lt;&Backslash;h1&gt;</h1>
+    <h2>this is heading&lt;&Backslash;h2&gt;</h2>
+    <h3>this is heading&lt;&Backslash;h3&gt;</h3>
+    <h4>this is heading&lt;&Backslash;h4&gt;</h4>
+    <h5>this is heading&lt;&Backslash;h5&gt;</h5>
+    <h6>this is heading&lt;&Backslash;h6&gt;</h6>
+    
+    <hr>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae dolores blanditiis ducimus, sed et animi labore magni mollitia quam excepturi incidunt nobis adipisci dolorem voluptas tempore commodi eveniet quod numquam!</p>
+
+    <hr>
+    <h1>Shopping list</h1>
+    <ul >
+        <li>Milk</li>
+        <li>Butter</li>
+        <li>Bread</li>
+        <li>Jam</li>
+        <li>Peanut butter</li>
+        <li>cookies</li>
+    </ul>
+    <hr>
+    <h1>Maggie Recepie</h1>
+    <ol type="I">
+        <li>Take a boul</li>
+        <li>On Stove</li>
+        <li>Put a cup of water</li>
+        <li>Boil it for 2  minutes</li>
+        <li>Put maggie in Boul</li>
+        <li>Add some veggies too for extra flavour:-
+            <ol>
+                <li>Peas</li>
+                <li>Onion</li>
+                <li>Carrot</li>
+            </ol>
+        </li>
+        <li>Add spices</li>
+        <li>Boil it for 5 minutes</li>
+        <li>TADA ! YOUR MAGGIE is READY TO EAT ENJIY:)</li>
+    </ol>
+    <hr>
+    <a href="https://www.google.com" target="_blank">Google</a>
+    <a href="1.html" target="_blank">Gateway to File 1</a>
+    <a href="MY ALL DOCS.pdf" target="_blank"><h4>To open my all documents</h4></a>
+    <a href="https://drive.google.com/drive/u/0/my-drive" target="_blank">Open GDrive</a>
+    <hr>
+    <img src="img.jpg" alt="Bhavesh photo" width="300" height="360">
+    <hr>
+    <video src="video1.mp4" width="400" controls loop muted poster="img.jpg"></video>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/b5-dh3f4MaY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe src="https://www.sarkariresult.com" frameborder="0" width="600" height="600"></iframe>
+    <hr>
+    <table>
+        <thead>
+            <tr>
+                <th>Subject</th>
+                <th>Theory</th>
+                <th>Practicle</th>
+                <th>Total marks</th>
+                <th>Grade</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>English</td>
+                <td>78</td>
+                <td>-</td>
+                <td>78</td>
+                <td>B1</td>
+            </tr>
+            <tr>
+                <td>Maths</td>
+                <td>90</td>
+                <td>-</td>
+                <td>90</td>
+                <td>A</td>
+            </tr>
+            <tr>
+                <td>Physics</td>
+                <td>45</td>
+                <td>49</td>
+                <td>94</td>
+                <td>A</td>
+            </tr>
+            <tr>
+                <td>Chemistry</td>
+                <td>38</td>
+                <td>45</td>
+                <td>83</td>
+                <td>B1</td>
+            </tr>
+            <tr>
+                <td>Hindi</td>
+                <td>98</td>
+                <td>-</td>
+                <td>98</td>
+                <td>A</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <th colspan="5">Result : PASS</th>
+        </tfoot>
+    </table>
+    <hr>
+    <h1>Students Exam Record</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>NAME</th>
+                <th>ROLL NO.</th>
+                <th>MARKS</th>
+
+            </tr>
+
+        </thead>
+        <tbody>
+            <tr>
+                <td>Bhavesh</td>
+                <td>010</td>
+                <td>494</td>
+            </tr>
+            <tr>
+                <td>Kanchan</td>
+                <td>017</td>
+                <td>499</td>
+            </tr>
+            <tr>
+                <td>Kajal</td>
+                <td>024</td>
+                <td>491</td>
+            </tr>
+            <tr>
+                <td>Babita</td>
+                <td>009</td>
+                <td>509</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <th colspan="3">PASSING MARKS OUT OF 600: 300 </th>
+        </tfoot>
+    </table>
+</body>
+</html>
